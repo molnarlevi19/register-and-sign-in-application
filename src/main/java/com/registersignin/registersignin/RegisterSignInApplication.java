f5c33da80efc7e14b0e76a508ba132b21e6ebf3b
Molnár Levente <112870372+molnarlevi19@users.noreply.github.com>
2024-01-10 09:36:42 +0100

    initialize starter code

---
+++ src/main/java/com/registersignin/registersignin/RegisterSignInApplication.java
@@ -0,0 +1,13 @@
+package com.registersignin.registersignin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterSignInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterSignInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/registersignin/registersignin/RegisterSignInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registersignin.registersignin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterSignInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
